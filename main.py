ca5942fafe8f15e2372b307e7063ce3012a1a433
Gabriel Miguel <gabrielmiguel@Mac-mini-de-Rolando.local>
2026-07-04 09:15:12 -0500

    segundo

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import random
 numero = int(input("Adivina un número del 1 al 10: "))
 numero2 = random.randint(1,10)
 
+puntaje = 0
+
 if numero == numero2:
     print("Has adivinado el numero")
+    puntaje += 1
+    print(f"Has adivinado el numero, tienes {puntaje} puntos")
 else:
     print(f"No has adivinado el numero, el numero era {numero2}")                    
